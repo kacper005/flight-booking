53a81c1c5f8ec18c048c5537f33220095d770319
Kacper <kacperm@stud.ntnu.no>
2025-01-21 10:23:52 +0100

    Installed and configured shadcn and tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@/components/ui/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button>Click <Meta:kw></Meta:kw></Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
