6ac65498d03441e716a7dc08b49f54511b8f0a13
Kacper <kacperm@stud.ntnu.no>
2025-01-31 08:18:47 +0100

    Changed the file structure

--- frontend/src/custom_components/organisms/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Header = () => {
-  return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/login">Login</Link>
-    </nav>
-  );
-}
---
+++ src/custom_components/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+    </div>
+  );
+};
